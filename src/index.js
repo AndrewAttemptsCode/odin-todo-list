1dde48ac99462b97dda5c71470d89e756c75f2a9
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-07-22 15:56:46 +0100

    Add sidebar content module to dom loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./style.css";
 import { loadTemplate } from "./template";
 import { loadHeaderContent } from "./headerContent";
+import { loadSideBarContent } from "./sideBarContent";
 
 document.addEventListener("DOMContentLoaded", () => {
     loadTemplate();
     loadHeaderContent();
+    loadSideBarContent();
 })
