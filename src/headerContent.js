e83e068f8e51a4ff310c6f9b2da4842a458a910a
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-07-22 14:37:44 +0100

    Add logo container and image

--- src/headerContent.js
+++ src/headerContent.js
@@ -1,8 +1,20 @@
+import checklistImage from "./images/checklist.svg";
+
 export function loadHeaderContent() {
     const header = document.querySelector(".header");
 
-    const logoText = document.createElement("div");
+    const logoContainer = document.createElement("div");
+    logoContainer.classList.add("logo-container");
+    header.appendChild(logoContainer);
+    
+    const logoImage = document.createElement("img");
+    logoImage.classList.add("logo-image");
+    logoImage.src = checklistImage;
+    logoImage.alt = "checklist image";
+    logoContainer.appendChild(logoImage);
+
+    const logoText = document.createElement("h1");
     logoText.classList.add("logo-text");
     logoText.textContent = "Project Planner";
-    header.appendChild(logoText);
+    logoContainer.appendChild(logoText);
 }
