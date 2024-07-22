f71b7a30e293e853492fbdfe55cfb10af411d8cd
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-07-22 14:04:49 +0100

    Add header content module to the DOM event listener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import { loadTemplate } from "./template";
+import { loadHeaderContent } from "./headerContent";
 
 document.addEventListener("DOMContentLoaded", () => {
     loadTemplate();
+    loadHeaderContent();
 })
