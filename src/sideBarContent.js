28b0655611650e0e740b0ada53641a197c3f03de
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-07-22 15:56:00 +0100

    Add initial content layout

---
+++ src/sideBarContent.js
@@ -0,0 +1,21 @@
+export function loadSideBarContent() {
+    const sideBar = document.querySelector(".side-bar");
+
+    const controlsContainer = document.createElement("div");
+    controlsContainer.classList.add("sidebar-controls-container");
+    sideBar.appendChild(controlsContainer);
+
+    const title = document.createElement("h1");
+    title.classList.add("sidebar-title");
+    title.textContent = "Projects";
+    controlsContainer.appendChild(title);
+
+    const addButton = document.createElement("button");
+    addButton.classList.add("add-project-button");
+    addButton.textContent = "+";
+    controlsContainer.appendChild(addButton);
+
+    const projectContainer = document.createElement("div");
+    projectContainer.classList.add("project-container");
+    sideBar.appendChild(projectContainer);
+}
