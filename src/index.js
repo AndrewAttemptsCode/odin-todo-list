bad2211d7927bb41d8e18a210956df2e81933df0
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-07-23 15:53:46 +0100

    Add project form to dom listener

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import "./style.css";
 import { loadTemplate } from "./template";
 import { loadHeaderContent } from "./headerContent";
 import { loadSideBarContent } from "./sideBarContent";
+import { loadProjectForm } from "./projectForm";
 
 document.addEventListener("DOMContentLoaded", () => {
     loadTemplate();
     loadHeaderContent();
     loadSideBarContent();
+    loadProjectForm();
 })
