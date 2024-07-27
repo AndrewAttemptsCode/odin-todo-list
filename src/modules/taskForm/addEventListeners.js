0b35634322f17723b5f76a9e13eb3513c446dcd9
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-07-27 18:27:21 +0100

    Add task to project object event listener to submit button

--- src/modules/taskForm/addEventListeners.js
+++ src/modules/taskForm/addEventListeners.js
@@ -1,8 +1,31 @@
-export function taskFormEventListeners() {
+import { addTaskToProject } from "../sideBarContent/projectData";
+
+export function taskFormEventListeners(selectedProjectName) {
     const closeButton = document.querySelector(".task-dialog .menu-close");
     const dialog = document.querySelector(".task-dialog");
 
     closeButton.addEventListener("click", () => {
         dialog.close();
+    });
+
+    const taskForm = document.querySelector(".task-form");
+    taskForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const taskTitle = taskForm.taskTitle.value;
+        const taskDescription = taskForm.taskDescription.value;
+        const taskPriority = taskForm.taskPriority.value;
+        const taskDueDate = taskForm.taskDueDate.value;
+
+        const task = {
+            title: taskTitle,
+            description: taskDescription,
+            priority: taskPriority,
+            dueDate: taskDueDate
+        };
+
+        addTaskToProject(selectedProjectName, task);
+        dialog.close();
+        taskForm.reset();
     });
 }
