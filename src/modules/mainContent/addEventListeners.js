d379d4f746071c6474d3725f1acec4e4d3e9371e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-07-27 14:38:38 +0100

    Remove taskformeventlisteners from the add task click event, apply it once for when he form is loaded

--- src/modules/mainContent/addEventListeners.js
+++ src/modules/mainContent/addEventListeners.js
@@ -1,12 +1,15 @@
 import { loadTaskForm } from "../taskForm/loadTaskForm";
+import { taskFormEventListeners } from "../taskForm/addEventListeners";
 
-export function taskFormEventListeners() {
+export function mainEventListeners() {
     const addTaskButton = document.querySelector(".add-task-container");
 
     loadTaskForm();
-
+    taskFormEventListeners();
+    
     addTaskButton.addEventListener("click", () => {
         const dialog = document.querySelector(".task-dialog");
         dialog.showModal();
     });
+
 }
