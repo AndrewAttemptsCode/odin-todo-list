639ddfbb1d3f37c918b1fe56a8cfce84bd6e53bc
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-07-27 16:36:28 +0100

    Add form inputs

--- src/modules/taskForm/loadTaskForm.js
+++ src/modules/taskForm/loadTaskForm.js
@@ -20,21 +20,74 @@ export function loadTaskForm() {
     menuBar.appendChild(close);
 
     const form = document.createElement("form");
-    form.classList.add("project-form");
+    form.classList.add("task-form");
     form.method = "dialog";
     dialog.appendChild(form);
 
-    const labelName = document.createElement("label");
-    labelName.textContent = "Task Name: ";
-    form.appendChild(labelName);
+    // Form Fields
 
-    const inputName = document.createElement("input");
-    inputName.classList.add = "project-name";
-    inputName.type = "text";
-    inputName.name = "taskName";
-    inputName.required = true;
-    inputName.maxLength = 20;
-    form.appendChild(inputName);
+    const labelTitle = document.createElement("label");
+    labelTitle.setAttribute("for", "taskTitle");
+    labelTitle.textContent = "Title:";
+    form.appendChild(labelTitle);
+
+    const taskTitle = document.createElement("input");
+    taskTitle.id = "taskTitle";
+    taskTitle.classList.add("task-form-input");
+    taskTitle.type = "text";
+    taskTitle.name = "taskTitle";
+    taskTitle.required = true;
+    taskTitle.maxLength = 20;
+    form.appendChild(taskTitle);
+
+    const labelDescription = document.createElement("label");
+    labelDescription.setAttribute("for", "taskDescription");
+    labelDescription.textContent = "Description:"
+    form.appendChild(labelDescription);
+
+    const taskDescription = document.createElement("textarea");
+    taskDescription.id = "taskDescription";
+    taskDescription.classList.add("task-description");
+    taskDescription.name = "taskDescription";
+    taskDescription.required = false;
+    taskDescription.maxLength = 100;
+    form.appendChild(taskDescription);
+
+    const labelPriority = document.createElement("label");
+    labelPriority.setAttribute("for", "taskPriority");
+    labelPriority.textContent = "Priority:";
+    form.appendChild(labelPriority);
+
+    const taskPriority = document.createElement("select");
+    taskPriority.id = "taskPriority";
+    taskPriority.classList.add("task-form-input");
+    taskPriority.name = "taskPriority";
+    taskPriority.required = true;
+    form.appendChild(taskPriority);
+
+    const lowPriority = document.createElement("option");
+    lowPriority.textContent = "Low";
+    taskPriority.appendChild(lowPriority);
+
+    const mediumPriority = document.createElement("option");
+    mediumPriority.textContent = "Medium";
+    taskPriority.appendChild(mediumPriority);
+
+    const highPriority = document.createElement("option");
+    highPriority.textContent = "High";
+    taskPriority.appendChild(highPriority);
+
+    const labelDate = document.createElement("label");
+    labelDate.setAttribute("for", "taskDueDate");
+    labelDate.textContent = "Due Date:";
+    form.appendChild(labelDate);
+
+    const taskDueDate = document.createElement("input");
+    taskDueDate.type = "date";
+    taskDueDate.id = "taskDueDate";
+    taskDueDate.classList.add("task-form-input");
+    taskDueDate.name = "taskDueDate";
+    form.appendChild(taskDueDate);
 
     const buttonSubmit = document.createElement("button");
     buttonSubmit.type = "submit";
